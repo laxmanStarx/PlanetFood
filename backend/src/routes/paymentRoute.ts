02e63de8775ad45b5496b4635c75928aea0576ac
vaibhav <laxmanvaibhav41@gmail.com>
2025-07-27 01:16:56 +0530

    frontend designs

--- backend/src/routes/paymentRoute.ts
+++ backend/src/routes/paymentRoute.ts
@@ -262,7 +262,7 @@ async function generateRecommendationsAndUpdateDB(userId: string) {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `${process.env.CLIENT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.FRONTEND_URL}/cancel`,
       metadata: {
         userId,
