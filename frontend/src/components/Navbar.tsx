46e2f087d8bdeffd39109106dccd156c0cd6956b
vaibhav <laxmanvaibhav41@gmail.com>
2026-02-05 22:51:46 +0530

    fixed issuesunflowershasfhoo

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -70,7 +70,7 @@ return (
       {/* Nav Links (Desktop) - REMOVE sm:flex */}
       <div className="hidden lg:flex items-center space-x-6">
         <span onClick={() => navigate("/aboutus")} className="cursor-pointer">
-          About Us
+          About Us hello
         </span>
         <button onClick={() => setShowRateUs(true)} className="cursor-pointer">
           Rate Us
